7f04aacef0ef95f803f67a1fa3fab8b4e296cf7b
YYYuriyyy <googlposhta71@gmail.com>
2023-07-24 19:25:16 +0300

    DZ_1

---
+++ DZ_1.py
@@ -0,0 +1,8 @@
+namber_1 = input("Введіть перше число:")
+namber_2 = input("Введіть друге число:")
+namber_3 = input("Введіть трете число:")
+
+sum_nambers = namber_1 + namber_2 + namber_3
+
+print(sum_nambers)
+
